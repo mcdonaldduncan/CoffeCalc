4315dc71a7aac8801485f736cb5194408dd6525a
mcdonaldduncan <380939@colum.edu>
2022-12-16 06:43:00 -0600

    Initial commit

---
+++ Coffeemachine.cpp
@@ -0,0 +1,36 @@
+﻿// Coffeemachine.cpp : Defines the entry point for the application.
+//
+
+#include "Coffeemachine.h"
+#include <string>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+	int totalH2O = 0;
+	int ozpercup = 2;
+
+	// Read
+	string temp;
+	ifstream ReadFile("input.txt");
+
+	while (getline(ReadFile, temp))
+	{
+		totalH2O += stoi(temp);
+	}
+
+	// Write
+	ofstream WriteFile("output.txt");
+
+	WriteFile << "Total Water: " << totalH2O << " cups" << endl;
+	WriteFile << "Total Grounds: " << totalH2O * ozpercup << " oz" << endl;
+
+	WriteFile.close();
+
+	cout << "Total Water: " << totalH2O << " cups" << endl;
+	cout << "Total Grounds: " << totalH2O * ozpercup << " oz" << endl;
+
+}
